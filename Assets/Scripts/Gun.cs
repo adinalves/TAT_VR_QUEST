9e3b3f2185aab4babdaada13fab0292c6154ffde
Adinê Alves <adinealves@172-15-2-67.lightspeed.stlsmo.sbcglobal.net>
2021-09-14 12:11:38 -0300

    first

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+
+
+public class Gun : MonoBehaviour {
+
+    public Transform bulletPrefab;
+
+    Animator    animator;
+    AudioSource audioSource;
+
+    FirstPersonCamera fpsCam;
+
+    ShotEffects shootEffects;
+
+    Transform shotSpawn;
+    Transform shellSpawn;
+
+    string gunName = "Pistol";
+    public int bulletCount = 15;
+    public GameObject CanvasResultado;
+    
+    
+
+    private void Awake() {
+
+        animator    = transform.Find(gunName).GetComponent<Animator>();
+        audioSource = transform.GetComponent<AudioSource>();
+
+        fpsCam     = transform.parent.GetComponent<FirstPersonCamera>();
+        shotSpawn  = transform.Find("shotSpawn");
+        shellSpawn = transform.Find("shellSpawn");
+
+        shootEffects = GetComponent<ShotEffects>();
+
+    }
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+        
+        if(Input.GetKeyDown(KeyCode.Mouse0) && Cursor.visible == false) {
+
+        
+            bulletCount--;
+            
+            animator.Play("Shoot", -1, 0);
+
+            audioSource.Play();
+
+            shootEffects.MuzzleFlash(shotSpawn.position, shotSpawn.rotation);
+            shootEffects.Shell(shellSpawn.position, shellSpawn.rotation);
+
+            // implementação do tiro vem aqui
+            ShootBullet();
+
+        } 
+
+        if(CanvasResultado.activeSelf)
+        {
+            
+            bulletCount = 15;
+        
+
+        }
+        
+        
+
+    }
+
+    void ShootBullet() {
+
+        Transform bulletObj = Instantiate(bulletPrefab, shotSpawn.position, shotSpawn.rotation);
+        Destroy(bulletObj.gameObject, 10f);
+        
+        RaycastHit hitInfo;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.GetForwardDirection(), out hitInfo, Mathf.Infinity, LayerMask.GetMask("hittable"))) {
+
+            
+            bulletObj.GetComponent<Bullet>().SetDirection((hitInfo.point - shotSpawn.position).normalized);
+            
+
+        } else {
+            bulletObj.GetComponent<Bullet>().SetDirection(fpsCam.GetForwardDirection());
+        }
+
+    }
+
+
+
+
+    public int getBullet()
+    {
+        return bulletCount;
+    }
+
+   
+   
+
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Target : MonoBehaviour, IShotHit {
+
+    Animator animator;
+    public int acertos = 0;
+    public List<Vector3> vetores = new List<Vector3>();
+    public float mediaX = 0;
+    public float mediaY = 0;
+    
+    
+
+    
+
+    void Start() {
+        
+          
+        animator = transform.parent.GetComponent<Animator>();
+    }
+
+    public void Hit(Vector3 direction) {
+        
+        animator.Play("TargetShot", -1, 0);
+        
+        acertos ++;
+        Debug.Log(acertos);
+        
+    }
+
+    public void Posicao(Vector3 coordenada){
+            Debug.Log(coordenada);
+            vetores.Add(coordenada);
+            mediaX += coordenada[0];
+            mediaY += coordenada[1];
+           
+    }
+
+    public double Dispersao()
+    {
+        double sigmaX = 0;
+        double sigmaY = 0;
+        double sigmaResultante;
+        mediaX = mediaX/acertos;
+        mediaY = mediaY/acertos;
+
+        if(acertos != 0) {
+        for(int i=0;i<acertos;i++)
+        {
+            sigmaX += (vetores[i][0] - mediaX)*(vetores[i][0] - mediaX);
+            
+            sigmaY += (vetores[i][1] - mediaY)*(vetores[i][1] - mediaY);
+            
+        }
+        
+        sigmaResultante = Math.Sqrt(sigmaX/acertos + sigmaY/acertos);
+
+        acertos = 0;
+        mediaY = 0;
+        mediaX = 0;
+        vetores.Clear();
+
+        return sigmaResultante;
+        }
+        acertos = 0;
+        mediaY = 0;
+        mediaX = 0;
+        vetores.Clear();
+        return 0;
+    }
+
+    public int getHit(){
+        return acertos;
+    }
+
+    
+
+
+}
+
